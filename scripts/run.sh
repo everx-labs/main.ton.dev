dd533159cf38d307ac77558eec446384b92bd9f6
Artem Ryabov <60089908+artemr-tl@users.noreply.github.com>
2021-01-19 11:21:02 +0300

    Cumulate output in node.log

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,6 +9,6 @@ echo "INFO: log file = ${TON_WORK_DIR}/node.log"
 
 # shellcheck disable=SC2086
 "${TON_BUILD_DIR}/validator-engine/validator-engine" ${ENGINE_ADDITIONAL_PARAMS} \
-    -C "${TON_WORK_DIR}/etc/ton-global.config.json" --db "${TON_WORK_DIR}/db" > "${TON_WORK_DIR}/node.log" 2>&1 &
+    -C "${TON_WORK_DIR}/etc/ton-global.config.json" --db "${TON_WORK_DIR}/db" >> "${TON_WORK_DIR}/node.log" 2>&1 &
 
 echo "INFO: start TON node... DONE"
